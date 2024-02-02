260dce9d094f2d21a4d3bb57bb2fdb3d83c4298c
Peter Huisman <Peter.Huisman@pggm.nl>
2024-02-02 08:55:21 +0100

    switched private and public keys

--- scripts/api/index.js
+++ scripts/api/index.js
@@ -3,30 +3,33 @@ const fs = require("fs");
 const bodyParser = require("body-parser");
 const crypto = require("crypto");
 
-const publicPem = `-----BEGIN PUBLIC KEY-----
-MIGeMA0GCSqGSIb3DQEBAQUAA4GMADCBiAKBgH115JfDEMa3OW7bQMwY80M6jzm8
-I1Si+1NNeagnbbVTXP/MQ9eAN/OK+Ah5Wzv2kQgARMfSMbPlOsO3kY+Zks7gSTtT
-8aTxGcKIEjYsR/lTof3iBDEwHvRpcF2iNjwzSawZTVu5e/cyhnDDrlur+MhMgPMm
-YCtYFFtnpbDkMZBpAgMBAAE=
------END PUBLIC KEY-----`;
+const privatePem = `-----BEGIN RSA PRIVATE KEY-----
+MIICXAIBAAKBgH115JfDEMa3OW7bQMwY80M6jzm8I1Si+1NNeagnbbVTXP/MQ9eA
+N/OK+Ah5Wzv2kQgARMfSMbPlOsO3kY+Zks7gSTtT8aTxGcKIEjYsR/lTof3iBDEw
+HvRpcF2iNjwzSawZTVu5e/cyhnDDrlur+MhMgPMmYCtYFFtnpbDkMZBpAgMBAAEC
+gYA+LJSSWQsRT2/Y7jMYcizr3jNoa0IfCX3/dF+b455Mw/lMkw/z1gjkWrQ8jteV
+ycVp76gmVpZnDmym3Wv3fCXyRXPWSq3EpCzygZLprWfQsDTOlH8ABcWAs8Oga94W
+cvNHjGzWZNM+SzAyuz/kxy8GuaBRpAcBdDlChfqdkJ+NAQJBAOHNw+4WpkCX/i2u
+i+mjIFfk+DsHK4yussdiP/vKo79EoKMPX1MukNWjBW2IeReKXtS7uMHDkKVLKkV6
+OecOhFkCQQCOPPJDMFVlD6h2J7v6JZyGuOrHf4RAwaELsMnViVMs/Kd0QLTtC2aY
+jZ0X+8spN+Yk73eOfjLZCm905JtdEiqRAkEAg2UxNkKHy96mUf7X+8So9XyP1gl+
+FgcykUNi6CoqzwooT7qKReU68pZCelKH5GLoe/IguOAMM6NhnbxaJVIVQQJASHAB
+cPZMhwtSX9ocgWhmLrY92xu13sS9n5aJM5acJW0GWs4ZVh9YQJjeSDiTXop5SpYp
+7QeYHbCS0pUaCmwWAQJBAMio6f596eC4aQfoCdrQGpRuXF8VAu+FH1qpUFSXKd9+
+5M+4bVe9VG4F/evrT3ChB9XxUENFIzRYtHxIawrG0t8=
+-----END RSA PRIVATE KEY-----`;
 
-// const privateKey = crypto.createPrivateKey(privatePem);
-const publicKey = crypto.createPublicKey(publicPem);
+const privateKey = crypto.createPrivateKey(privatePem);
 
 module.exports = (app) => {
   app.use(bodyParser.json());
 
   app.get("/token", (req, res) => {
     const data = fs.readFileSync('./scripts/api/token.json', 'utf8');
-    // const data = JSON.stringify({"bsn": "1234567890"});
-    const encryptedData = crypto.publicEncrypt(
+    const encryptedData = crypto.privateEncrypt(
       {
-        key: publicKey,
-        padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
-        // oaepHash: "sha256",
+        key: privateKey,
       },
-      // We convert the data string to a buffer using `Buffer.from`
-      // eslint-disable-next-line no-undef
       Buffer.from(data)
     );
 
